63f9679f6fd74e0860f930da5f7461dd13feb2d1
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-18 14:36:13 +0200

    Inserito File JS e modificati 2 titoli

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('RecentlyJs').innerHTML = 'Recently played but written in JavaScript'
+
+document.getElementById('CreateJs').innerHTML = 'Creato per Crescenzo De Martino in JavaScript'
